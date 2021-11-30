1e7eeaf63e5e27ff1e6f86aa819e9c06494cabf0
Aditya Cahyo <57148744+aditydcp@users.noreply.github.com>
2021-11-30 11:54:38 +0700

    apply player movements
    horizontal + vertical movement. added respawn for quick testing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,75 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 10f;
+    public float jumpForce = 10f;
+    public float horizontalInput;
+    public float verticalInput;
+    private Transform groundCheck;
+    //private Transform groundCheckLeft;
+    //private Transform groundCheckRight;
+    const float groundRadius = .2f;
+    public bool isGrounded;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D m_Rigidbody2D;
 
     // Start is called before the first frame update
     void Start()
     {
+        // set up
+        groundCheck = transform.Find("GroundCheck");
+        //groundCheckLeft = transform.Find("GroundCheck_left");
+        //groundCheckRight = transform.Find("GroundCheck_right");
         spriteRenderer = GetComponent<SpriteRenderer>();
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis("Horizontal") > 0)
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        GroundCheck();
+        Movement();
+        Respawn();
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = false;
+
+        // isGrounded is true whenever something enters within the GroundCheck radius
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundRadius);
+        for (int i = 0; i < colliders.Length; i++)
         {
-            spriteRenderer.flipX = true;
+            if (colliders[i].gameObject != gameObject)
+                isGrounded = true;
         }
-        if (Input.GetAxis("Horizontal") < 0)
+    }
+
+    private void Movement()
+    {
+        if (horizontalInput != 0)
         {
-            spriteRenderer.flipX = false;
+            // flips the character
+            spriteRenderer.flipX = (horizontalInput > 0 ? true : false);
+            // moves the character
+            m_Rigidbody2D.velocity = new Vector2(horizontalInput * moveSpeed, m_Rigidbody2D.velocity.y);
+        }
+
+        //if((verticalInput > 0 || Input.GetButtonDown("Jump")) && isGrounded)
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+        {
+            isGrounded = false;
+            m_Rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+    private void Respawn()
+    {
+        if(transform.position.y < -10)
+        {
+            transform.position = new Vector2(0, 0);
         }
     }
 }
