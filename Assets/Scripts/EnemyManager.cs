b5b35ae0ffc7354c57635b8e70860fc129e8bcdc
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-12-01 17:56:07 +0700

    Implement enemy revival.

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -6,9 +6,14 @@ public class EnemyManager : MonoBehaviour
 {
     public int MaxHealth = 100;
     public int CurrentHealth = 100;
+    public int regenerationPerSecond = 5;
 
     public ProgressBar HealthBar;
     public GameObject Turret;
+
+    public GameObject TurretPrefab;
+
+    public IEnumerator RegenerationCoroutine;
 
     // Start is called before the first frame update
     void Start()
@@ -23,16 +28,55 @@ public class EnemyManager : MonoBehaviour
 
     }
 
+    private IEnumerator Regenerate()
+    {
+        while (CurrentHealth < MaxHealth)
+        {
+            if (CurrentHealth + regenerationPerSecond < MaxHealth)
+            {
+                CurrentHealth += regenerationPerSecond;
+            }
+            else
+            {
+                CurrentHealth = MaxHealth;
+            }
+
+            HealthBar.SetCurrentProgress(CurrentHealth);
+
+            yield return new WaitForSeconds(1f);
+        }
+
+        Revive();
+    }
+
+    private void Revive()
+    {
+        Turret = Instantiate(TurretPrefab, transform);
+
+        Turret.transform.localPosition = new Vector2(0.01f, -0.87f);
+    }
+
     public void TakeDamage(int damageAmount)
     {
-        CurrentHealth -= damageAmount;
+        if (CurrentHealth - damageAmount > 0)
+        {
+            CurrentHealth -= damageAmount;
+        }
+        else
+        {
+            CurrentHealth = 0;
+        }
 
-        if (CurrentHealth < 0 && Turret != null)
+        if (CurrentHealth == 0 && Turret != null)
         {
             var TurretRigidbody = Turret.GetComponent<Rigidbody2D>();
             TurretRigidbody.constraints = RigidbodyConstraints2D.None;
 
+            StartCoroutine(Regenerate());
+
             Destroy(Turret, 10f);
+
+            Turret = null;
         }
 
         HealthBar.SetCurrentProgress(CurrentHealth);
