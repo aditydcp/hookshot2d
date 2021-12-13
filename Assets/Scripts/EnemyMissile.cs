064810a49d792ebd825b2953eac688b6c91c9249
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-12-13 15:20:05 +0700

    Implement missile.

---
+++ Assets/Scripts/EnemyMissile.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMissile : MonoBehaviour
+{
+    private Transform _target = null;
+
+    public float Speed = 10f;
+    public float RotationSpeed = 5f;
+    public Rigidbody2D MissileRigidBody;
+    public int SelfDestructDelaySeconds = 5;
+
+
+    void Start()
+    {
+        AcquireTarget();
+        Destroy(gameObject, SelfDestructDelaySeconds);
+    }
+
+    void FixedUpdate()
+    {
+        if (_target != null)
+        {
+            var turnDirection = (Vector2)_target.position - MissileRigidBody.position;
+
+            var rotation = Vector3.Cross(turnDirection.normalized, transform.up).z;
+
+            MissileRigidBody.angularVelocity = -rotation * RotationSpeed;
+        }
+
+        MissileRigidBody.velocity = transform.up * Speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var player = collision.gameObject.GetComponent<PlayerManager>();
+
+        if (player != null)
+        {
+            player.TakeDamage(10);
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void AcquireTarget()
+    {
+        var player = GameObject.Find("Player");
+        _target = player.transform;
+    }
+}
--- Assets/Scripts/EnemyShooting.cs
+++ Assets/Scripts/EnemyShooting.cs
@@ -6,13 +6,19 @@ public class EnemyShooting : MonoBehaviour
 {
     public Transform FiringPoint;
     public GameObject BulletPrefab;
+    
+    public float MinInitialShotDelay = 2f;
+    public float InitialShotDelayVariation = 2f;
+
+    public float MinShootingInterval = 5f;
+    public float ShootingIntervalVariation = 2f;
 
     public float BulletForce = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Shoot", 2f + Random.Range(0f, 2f), 5f + Random.Range(0f, 2f));
+        InvokeRepeating("Shoot", MinInitialShotDelay + Random.Range(0f, InitialShotDelayVariation), MinShootingInterval + Random.Range(0f, ShootingIntervalVariation));
     }
 
     void Shoot()
